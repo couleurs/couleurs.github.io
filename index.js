26c3688b32ad49f264c3a9a0949f5c1ae10ac1c4
Couleurs <johanismael@gmail.com>
2017-06-12 08:54:15 -0700

    fluid stuff working

---
+++ index.js
@@ -0,0 +1,33 @@
+var GLContext = require('./fluidwebgl/src/engine/gl/context')
+var Clock = require('./fluidwebgl/src/engine/clock').Clock
+var FluidSimulation = require('./fluidwebgl/src/main')
+
+// Canvas
+var canvas = document.createElement('canvas')
+canvas.style = 'position: absolute; width: 30%; height: 50%; top: 25%; left: 35%;'
+document.body.appendChild(canvas)
+
+// GL Context
+var gl = GLContext.initialize(canvas, {
+          context: {
+              depth: false
+          },
+          debug: false,
+
+          extensions: {
+              texture_float: true
+          }
+        },
+        function() {})
+
+var clock = new Clock(canvas)
+clock.start()
+
+var fluidSimulation = new FluidSimulation(gl, canvas)
+fluidSimulation.start()
+
+clock.ontick = function(dt) {
+  if (fluidSimulation.loaded) {
+    fluidSimulation.draw()
+  }
+}
